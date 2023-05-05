3461c8eba68c1286265a63e24a0d43ccdf5e25f3
IsoBones <isoboness@gmail.com>
2023-05-06 09:13:35 +1000

    Initial implementation of MRH-90
    Still needs lots of cleanup, but @discoADFU can now start to retex files while I do the config porting

---
+++ Addons/HAFM_NH90/config.cpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+	class HAFM_NH90
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"A3_Air_F",
+			"A3_Weapons_F",
+			"HAFM_NH90_Config"
+		};
+	};
+};
