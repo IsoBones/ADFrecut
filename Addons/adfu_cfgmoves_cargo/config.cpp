a47af06fcce3be78f933b0ea783da2df83dc9506
IsoBones <isoboness@gmail.com>
2024-07-28 00:11:41 +1000

    Nuke Main

--- Addons/adfu_cfgmoves_cargo/config.cpp
+++
@@ -1,9 +0,0 @@
-/* Header */
-#include "cfgPatches.hpp"
-
-/* Definitions & Inheritance */
-#include "\adfu_air_c130\basicDefines_A3.hpp"
-
-/* Configuration */
-#include "cfgMovesBasic.hpp"
-#include "cfgMovesMaleSdr.hpp"
--- meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 0;
-name = "@ADFRecut";
-timestamp = 5248852328127317664;
