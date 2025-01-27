3600bd986dd4833523a40b56c6202b6fcff4c528
Index <64417343+TDSIndex@users.noreply.github.com>
2025-01-27 12:46:38 +1000

    Updated MRAPs Config

--- Addons/ADF_MRAP/adfrc_bushmaster/config.cpp
+++ Addons/ADF_MRAP/adfrc_bushmaster/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
 		requiredVersion  = 1.60;
 
 		requiredAddons[] = { "A3_Data_F_Decade_Loadorder" };
-		units[]          = { };
+		units[]          = { "ADFRC_F_GWOT_Bushmaster_Unarmed_F", "ADFRC_F_GWOT_Bushmaster_RWS_M2_F" };
 		weapons[]        = { };
 	};
 };
