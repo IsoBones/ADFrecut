3269c1bcde3be99850c103b4dbb54c975c8c2440
Quiggsleyy <168166272+Quiggsleyy@users.noreply.github.com>
2025-05-23 14:58:31 +0800

    Added interior lights to core for vehicles.

--- Addons/ADF_Core/cfgVehicles.hpp
+++ Addons/ADF_Core/cfgVehicles.hpp
@@ -51,4 +51,54 @@ class CfgVehicles
 			init="(_this select 0) setFlagTexture '\ADF_Core\data\adfopfor_flag_co.paa'";
 		};
 	};
+	class Reflector_Base_F;
+
+      class ADFRC_White_Int_Light : Reflector_Base_F
+    {
+        author = "$STR_ADFRC_AUTHOR";
+        scope = 1;
+
+        class MarkerLights
+        {
+            class Light_1
+            {
+                color[] = { 1, 1, 1 };
+                ambient[] = { 1.6, 1.6, 1.6 };
+                intensity = 600;
+                name = "Light_1_pos";
+                blinking = 0;
+                useFlare = 0;
+                flareSize = 0.5;
+                flareMaxDistance = 1500;
+                activeLight = 1;
+                dayLight = 0;
+                drawLight = 0;
+                irLight = 0;
+
+                class Attenuation
+                {
+                    start = 0;
+                    constant = 0;
+                    linear = 10;
+                    quadratic = 60;
+                    hardLimitStart = 3;
+                    hardLimitEnd = 3;
+                };
+            };
+        };
+    };
+
+    class ADFRC_Red_Int_Light : ADFRC_White_Int_Light
+    {
+        author = "$STR_ADFRC_AUTHOR";
+
+        class MarkerLights : MarkerLights
+        {
+            class Light_1 : Light_1
+            {
+                color[] = { 1, 0, 0 };
+                ambient[] = { 1.6, 0, 0 };
+            };
+        };
+    };
 };
